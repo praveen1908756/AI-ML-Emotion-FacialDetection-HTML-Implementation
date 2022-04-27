d4ec82c2b81d2b3b7e64e04d0ea101fa8bf9d204
praveen1908756 <sppraveenkumar56@gmail.com>
2022-04-27 22:23:48 +0530

    Updated Project

---
+++ Face-and-Emotion-Recognition/s.php
@@ -0,0 +1,11 @@
+<html>
+    <head>
+        <title>Running a Python script</title>
+        <?php
+            echo shell_exec("python live-recog.py");
+        ?>
+        </head>
+    <body>
+
+    </body>
+</html>
